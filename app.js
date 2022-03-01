75ecb76af752a70a5aa2802a1d81e60734a21c72
Nadica Davcheva <nadicadavcheva@users.noreply.github.com>
2022-03-01 08:15:31 +0100

    Improve style and calculations (#2)
    * Invoice app changes
    
    * Fix calculations, improve css and style

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const createTableData = (rowRef, rowNumber, type, name) => {
     let tableData = document.createElement("td");
     rowRef.appendChild(tableData);
     let tableDataInput = document.createElement("input");
-    tableDataInput.classList.add("tableDataInput");
+    tableDataInput.classList.add("tableDataInput", name);
     tableDataInput.setAttribute("type", type);
     if (type === "number") {
         tableDataInput.setAttribute("min", 0);
@@ -23,11 +23,59 @@ const createTableData = (rowRef, rowNumber, type, name) => {
     }
     tableDataTxt.classList.add("tableDataTxt");
     tableDataTxt.id = `${name}-row-${rowNumber}`;
-    // tableDataTxt.style.backgroundColor = red;
     tableData.appendChild(tableDataTxt);
 
     tableDataInput.addEventListener("input", function () {
         tableDataTxt.innerText = this.value;
+    });
+};
+
+const createTextField = (rowRef, rowNumber, name) => {
+    let tableData = document.createElement("td");
+    rowRef.appendChild(tableData);
+    let tableDataTxt = document.createElement("span");
+
+    tableDataTxt.classList.add("tableDataTxt");
+    tableDataTxt.id = `${name}-row-${rowNumber}`;
+    tableData.appendChild(tableDataTxt);
+};
+
+const createTaxListData = (rowRef, rowNumber) => {
+    const id = `taxRate-row-${rowNumber}`;
+    let tableData = document.createElement("td");
+    rowRef.appendChild(tableData);
+
+    var select = document.createElement("select");
+    select.classList.add("tableDataInput", "taxRate");
+    select.name = "taxRate";
+    select.id = id;
+
+    var option = document.createElement("option");
+    option.value = "1.18";
+    option.text = "18%";
+    select.appendChild(option);
+
+    var option = document.createElement("option");
+    option.value = "1.05";
+    option.text = "5%";
+    select.appendChild(option);
+
+    var option = document.createElement("option");
+    option.value = "1";
+    option.text = "0%";
+    select.appendChild(option);
+
+    tableData.appendChild(select);
+
+    let tableDataTxt = document.createElement("span");
+    tableDataTxt.classList.add("tableDataTxt");
+    tableDataTxt.innerText = "18%";
+    tableDataTxt.id = id;
+    tableData.appendChild(tableDataTxt);
+
+    select.addEventListener("change", function (event) {
+        const { options, selectedIndex } = event.target;
+        tableDataTxt.innerText = options[selectedIndex].text;
     });
 };
 
@@ -41,21 +89,23 @@ let addProduct = function (e) {
     // Creating table data, elements within td and append td to tr
     let rowNumber = document.createElement("td");
     newRow.appendChild(rowNumber);
-    rowNumber.append(dataRow);
+    var rows = tBody.getElementsByTagName("tr");
+    rowNumber.append(rows.length);
     rowNumber.classList.add("rowNumber");
 
     createTableData(newRow, dataRow, "text", "desc");
     createTableData(newRow, dataRow, "number", "unitMeasure");
     createTableData(newRow, dataRow, "number", "quantity");
-    createTableData(newRow, dataRow, "number", "priceWithoutTax");
-    createTableData(newRow, dataRow, "number", "taxRate");
-    createTableData(newRow, dataRow, "number", "ddv");
+    createTextField(newRow, dataRow, "priceWithoutTax");
+    createTaxListData(newRow, dataRow);
+    createTextField(newRow, dataRow, "ddv");
 
     const amount = document.createElement("td");
     newRow.appendChild(amount);
     const amountTxt = document.createElement("span");
     amount.appendChild(amountTxt);
     const toggleBtnRow = document.createElement("td");
+    toggleBtnRow.classList.add("hidden-print");
     newRow.appendChild(toggleBtnRow);
 
     // Creating toggle check and edit button in the table rows
@@ -72,27 +122,35 @@ let addProduct = function (e) {
             newRow.classList.remove("notCalculated");
 
             amountTxt.style.display = "block";
-            // Math for the total number on the table footer
-            var unitMeasure = document.getElementById(
+
+            // Element objects
+            const unitMeasure = document.getElementById(
                 `unitMeasure-${this.id}`
-            ).innerText;
-            var quantity = document.getElementById(
-                `quantity-${this.id}`
-            ).innerText;
-            var priceWithoutTax = document.getElementById(
+            );
+            const quantity = document.getElementById(`quantity-${this.id}`);
+            const priceWithoutTax = document.getElementById(
                 `priceWithoutTax-${this.id}`
-            ).innerText;
-            var taxRate = document.getElementById(
-                `taxRate-${this.id}`
-            ).innerText;
-            var ddv = document.getElementById(`ddv-${this.id}`).innerText;
+            );
+            const taxRate = document.getElementById(`taxRate-${this.id}`);
+            const ddv = document.getElementById(`ddv-${this.id}`);
+
+            // Element value
+            const unitMeasureValue = unitMeasure.innerText;
+            const quantityValue = quantity.innerText;
+            const priceWithoutTaxValue = priceWithoutTax.innerText;
+            const taxRateValue = taxRate.value;
+            const ddvValue = ddv.innerText;
 
             let totalWithoutTaxes =
-                parseFloat(quantity) * parseFloat(priceWithoutTax);
-            let totalAmount =
-                totalWithoutTaxes +
-                (totalWithoutTaxes / 100) * parseFloat(taxRate) +
-                (totalWithoutTaxes / 100) * parseFloat(ddv);
+                parseFloat(unitMeasureValue) * parseFloat(quantityValue);
+            priceWithoutTax.innerText = totalWithoutTaxes.toFixed(2);
+
+            // price with ddv (unitMeasureValue * quantityValue) * taxRate [1,05 or 1,18 or 1]
+            const totalAmount = totalWithoutTaxes * taxRateValue;
+
+            // ddv = price with ddv - price without ddv
+            ddv.innerText = (totalAmount - totalWithoutTaxes).toFixed(2);
+
             amountTxt.innerText = totalAmount.toFixed(2);
             total.innerText = (
                 totalAmount + parseFloat(total.innerText)
@@ -118,6 +176,7 @@ let addProduct = function (e) {
     // Creating the delete button
     let deleteBtnRow = document.createElement("td");
     newRow.appendChild(deleteBtnRow);
+    deleteBtnRow.classList.add("hidden-print");
     let deleteBtn = document.createElement("button");
     deleteBtn.classList.add("deleteBtn");
     deleteBtn.setAttribute("id", `deleteBtnNum${dataRow}`);
@@ -132,13 +191,25 @@ let addProduct = function (e) {
             parseFloat(total.innerText) - parseFloat(deletedAmount)
         ).toFixed(2);
         if (!isNaN(newTotalAmount)) {
-            total.innerText = parseFloat(newVkupno).toFixed(2);
+            total.innerText = parseFloat(newTotalAmount).toFixed(2);
         }
+
         e.target.parentElement.parentElement.remove();
+
+        let rows = tBody.querySelectorAll("tr");
+
+        rows.forEach((row) => {
+            row.children[0].innerText = row.rowIndex;
+        });
     });
 };
 
 addProduct();
+
+let addNewProductBtn = document.getElementById("newProduct");
+addNewProductBtn.addEventListener("click", function () {
+    addProduct();
+});
 
 // Creating and giving function to reset and print button
 let resetBtn = document.getElementById("reset");
@@ -152,36 +223,12 @@ resetBtn.addEventListener("click", function () {
     addProduct();
 });
 
-let printBtn = document.getElementById("print");
+const printBtn = document.getElementById("btnPrint");
 printBtn.addEventListener("click", function () {
-    print();
-});
-
-// Add table row button
-let newProductBtn = document.getElementById("newProduct");
-newProductBtn.onclick = addProduct;
-
-// Toggle button for the companies and billing info
-let infoBtnDiv = document.querySelector(".infoBtnDiv");
-let infoBtn = document.createElement("button");
-infoBtnDiv.append(infoBtn);
-infoBtn.classList.add("checkBtnOnInfo");
-let pobaruvaInputs = document.querySelectorAll(".infoInputClass");
-let pobaruvaText = document.querySelector("h3");
-
-infoBtn.addEventListener("click", function () {
-    let done = pobaruvaText.toggleAttribute("disabled");
-    if (done) {
-        for (let i of pobaruvaInputs) {
-            i.disabled = true;
-        }
-        infoBtn.classList.remove("checkBtnOnInfo");
-        infoBtn.classList.add("editBtnOnInfo");
-    } else {
-        for (let i of pobaruvaInputs) {
-            i.disabled = false;
-        }
-        infoBtn.classList.remove("editBtnOnInfo");
-        infoBtn.classList.add("checkBtnOnInfo");
+    let allTableRows = document.querySelectorAll(".checkBtn");
+    for (let tableRow of allTableRows) {
+        tableRow.click();
     }
+
+    print();
 });
